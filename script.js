89694d43beece7fe38a60e19f02282c87f52299d
Thiago <127748415+Thiago00111000@users.noreply.github.com>
2024-08-23 17:57:10 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -18,3 +18,20 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+//--------------------------------------------------------------------
+
+function atualizarRelogio() {
+    const agora = new Date();
+    const hora = String(agora.getHours()).padStart(2, '0');
+    const minuto = String(agora.getMinutes()).padStart(2, '0');
+    const segundo = String(agora.getSeconds()).padStart(2, '0');
+
+    document.getElementById('hora').textContent = hora;
+    document.getElementById('minuto').textContent = minuto;
+    document.getElementById('segundo').textContent = segundo;
+}
+
+setInterval(atualizarRelogio, 1000);
+
+atualizarRelogio();
